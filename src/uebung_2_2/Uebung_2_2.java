100a59d0603289fd51ca88decf0aede9aa29517b
15471 <15471@A001-02.nordakademie.intern>
2017-10-26 11:10:58 +0200

    Übung 2.2

---
+++ src/uebung_2_2/Uebung_2_2.java
@@ -0,0 +1,15 @@
+package uebung_2_2;
+
+public class Uebung_2_2 {
+	public static void main(String[] args) {
+		
+		double a = 8;
+		double b = 3;
+		
+		double x1 = (a + b) * (a + b );
+		double x2 = a * a + 2 * a * b + b * b;
+		
+		System.out.print(x1 + " = " + x2);
+		
+	}
+}
